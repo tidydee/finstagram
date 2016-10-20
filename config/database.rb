ab818ea6bc1e4b0f43235df3b77e6720b12cf69b
Dawid Spamer <dawidspamer@gmail.com>
2016-10-20 02:51:20 +0000

    Ready to Deploy to Heroku - Round2

--- config/database.rb
+++ config/database.rb
@@ -4,11 +4,28 @@ configure do
     ActiveRecord::Base.logger = Logger.new(STDOUT)
   end
 
-  set :database, {
-    adapter: "sqlite3",
-    database: "postgres://ltkgimpngchnga:kOnqWv2xb5FJNj5zh-InPBGk7l@ec2-50-17-255-49.compute-1.amazonaws.com:5432/d6dqhgh7pu1go"
-    #database: "db/db.sqlite3"
-  }
+  # set :database, {
+  #   adapter: "sqlite3",
+  #   database: "postgres://ltkgimpngchnga:kOnqWv2xb5FJNj5zh-InPBGk7l@ec2-50-17-255-49.compute-1.amazonaws.com:5432/d6dqhgh7pu1go"
+  #   #database: "db/db.sqlite3"
+  # }
+  
+  if Sinatra::Application.development?
+    set :database, {
+      adapter: "sqlite3",
+      database: "db/db.sqlite3"
+    }
+  else
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://ltkgimpngchnga:kOnqWv2xb5FJNj5zh-InPBGk7l@ec2-50-17-255-49.compute-1.amazonaws.com:5432/d6dqhgh7pu1go')
+    set :database, {
+      adapter: "postgresql",
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
 
   # Load all models from app/models, using autoload instead of require
   # See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
